11d514382bbaa56a8b060397cf74bc10e0d0b4a1
Adrian V <adrianval96@gmail.com>
2024-01-24 14:13:57 +0100

    add app to settings, perform first migration

--- products/models.py
+++ products/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     price = models.FloatField()
+
+    class Meta:
+        db_table = 'products'
