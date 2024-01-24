34f22ac9da6114c1a927194a8a0444502da73862
Adrian V <adrianval96@gmail.com>
2024-01-25 00:34:41 +0100

    run black formatted on migration file

--- products/migrations/0002_populate_initial_products_data.py
+++ products/migrations/0002_populate_initial_products_data.py
@@ -8,384 +8,305 @@ PRODUCTS_LIST = [
         "name": "Termo",
         "price": 5.0,
         "description": "Termo de IKEA, no funciona muy bien",
-        "tags": [
-            ""
-        ]
+        "tags": [""],
     },
     {
         "id": "3",
         "name": "Rat\u00f3n Logitech G403 HERO",
         "price": 35.0,
         "description": "Rat\u00f3n en muy buen estado",
-        "tags": [
-            "Electr\u00f3nica"
-        ]
+        "tags": ["Electr\u00f3nica"],
     },
     {
         "id": "2",
         "name": "Teclado Logitech G512 CARBON",
         "price": 100.0,
         "description": "Tipo de tecla Cherry-MX Marr\u00f3n",
-        "tags": [
-            "Electr\u00f3nica"
-        ]
+        "tags": ["Electr\u00f3nica"],
     },
     {
         "id": "4",
         "name": "Auriculares SONY WH-1000 XM3",
         "price": 180.0,
         "description": "",
-        "tags": [
-            "Electr\u00f3nica"
-        ]
+        "tags": ["Electr\u00f3nica"],
     },
     {
         "id": "5",
         "name": "Mochila 20L Quechua",
         "price": 8.0,
         "description": "Mancha de pintura (no queda mal)",
-        "tags": [
-            ""
-        ]
+        "tags": [""],
     },
     {
         "id": "6",
         "name": "Mochila 10L Quechua",
         "price": 4.0,
         "description": "",
-        "tags": [
-            ""
-        ]
+        "tags": [""],
     },
     {
         "id": "7",
         "name": "Mochila 60L Quechua",
         "price": 30.0,
         "description": "Un poco desgastada en el interior",
-        "tags": [
-            ""
-        ]
+        "tags": [""],
     },
     {
         "id": "8",
         "name": "Pantalla ASUS 23\u201d",
         "price": 90.0,
         "description": "",
-        "tags": [
-            "Electr\u00f3nica"
-        ]
+        "tags": ["Electr\u00f3nica"],
     },
     {
         "id": "9",
         "name": "Pantalla ACER 24.5\u201d 165Hz",
         "price": 135.0,
         "description": "",
-        "tags": [
-            "Electr\u00f3nica"
-        ]
+        "tags": ["Electr\u00f3nica"],
     },
     {
         "id": "10",
         "name": "Silla The Secret Lab",
         "price": 280.0,
         "description": "",
-        "tags": [
-            "Muebles"
-        ]
+        "tags": ["Muebles"],
     },
     {
         "id": "11",
         "name": "Guitarra el\u00e9ctrica Ibanez",
         "price": 150.0,
         "description": "Golpe en la parte inferior de la guitarra, sin ajustar, el jack tiene que ser arreglado",
-        "tags": [
-            ""
-        ]
+        "tags": [""],
     },
     {
         "id": "12",
         "name": "Guitarra ac\u00fastica Fender",
         "price": 105.0,
         "description": "",
-        "tags": [
-            ""
-        ]
+        "tags": [""],
     },
     {
         "id": "13",
         "name": "Pies de gato Boreal talla (42?)",
         "price": 45.0,
         "description": "Recien recauchutados",
-        "tags": [
-            ""
-        ]
+        "tags": [""],
     },
     {
         "id": "14",
         "name": "Adaptador original Apple Macbook",
         "price": 60.0,
         "description": "",
-        "tags": [
-            "Electr\u00f3nica"
-        ]
+        "tags": ["Electr\u00f3nica"],
     },
     {
         "id": "15",
         "name": "Adaptador IVANKY USB-C - HDMI",
         "price": 8.0,
         "description": "",
-        "tags": [
-            "Electr\u00f3nica"
-        ]
+        "tags": ["Electr\u00f3nica"],
     },
     {
         "id": "16",
         "name": "Cable + Adaptador macbook original 67W ultimos modelos",
         "price": 65.0,
         "description": "",
-        "tags": [
-            "Electr\u00f3nica"
-        ]
+        "tags": ["Electr\u00f3nica"],
     },
     {
         "id": "18",
         "name": "Amplificador ORANGE CRUSH 12W",
         "price": 50.0,
         "description": "Sin el adaptador de corriente",
-        "tags": [
-            "Electr\u00f3nica"
-        ]
+        "tags": ["Electr\u00f3nica"],
     },
     {
         "id": "19",
         "name": "Apple Earpods ",
         "price": 12.0,
         "description": "Sin usar",
-        "tags": [
-            "Electr\u00f3nica"
-        ]
+        "tags": ["Electr\u00f3nica"],
     },
     {
         "id": "20",
         "name": "Estanteria IKEA IVAR",
         "price": 45.0,
         "description": "",
-        "tags": [
-            "Muebles"
-        ]
+        "tags": ["Muebles"],
     },
     {
         "id": "22",
         "name": "Ajedrez de piedra",
         "price": 25.0,
         "description": "Con algunas piezas rotas y reconstruidas",
-        "tags": [
-            "Juegos de mesa",
-            "Muebles"
-        ]
+        "tags": ["Juegos de mesa", "Muebles"],
     },
     {
         "id": "23",
         "name": "Ventilador de pie Orbegozo",
         "price": 25.0,
         "description": "",
-        "tags": [
-            "Muebles"
-        ]
+        "tags": ["Muebles"],
     },
     {
         "id": "24",
         "name": "Plancha",
         "price": 15.0,
         "description": "",
-        "tags": [
-            "Muebles"
-        ]
+        "tags": ["Muebles"],
     },
     {
         "id": "25",
         "name": "Tabla planchar",
         "price": 5.0,
         "description": "",
-        "tags": [
-            "Muebles"
-        ]
+        "tags": ["Muebles"],
     },
     {
         "id": "26",
         "name": "Black Stories",
         "price": 5.0,
         "description": "",
-        "tags": [
-            "Juegos de mesa"
-        ]
+        "tags": ["Juegos de mesa"],
     },
     {
         "id": "27",
         "name": "Chips de poker",
         "price": 3.0,
         "description": "",
-        "tags": [
-            "Juegos de mesa"
-        ]
+        "tags": ["Juegos de mesa"],
     },
     {
         "id": "28",
         "name": "Ciudadelas",
         "price": 8.0,
         "description": "",
-        "tags": [
-            "Juegos de mesa"
-        ]
+        "tags": ["Juegos de mesa"],
     },
     {
         "id": "29",
         "name": "Plata",
         "price": 7.0,
         "description": "",
-        "tags": [
-            "Juegos de mesa"
-        ]
+        "tags": ["Juegos de mesa"],
     },
     {
         "id": "30",
         "name": "Quien es quien italiano",
         "price": 2.0,
         "description": "",
-        "tags": [
-            "Juegos de mesa"
-        ]
+        "tags": ["Juegos de mesa"],
     },
     {
         "id": "31",
         "name": "Tienda campa\u00f1a F&B 3pax.",
         "price": 50.0,
         "description": "",
-        "tags": [
-            "Camping"
-        ]
+        "tags": ["Camping"],
     },
     {
         "id": "32",
         "name": "Saco dormir 10\u00ba Quechua",
         "price": 60.0,
         "description": "",
-        "tags": [
-            "Camping"
-        ]
+        "tags": ["Camping"],
     },
     {
         "id": "33",
         "name": "Esterilla hinchable de putos locos",
         "price": 20.0,
         "description": "",
-        "tags": [
-            "Camping"
-        ]
+        "tags": ["Camping"],
     },
     {
         "id": "34",
         "name": "Lampara campamento Quechua",
         "price": 15.0,
         "description": "",
-        "tags": [
-            "Camping"
-        ]
+        "tags": ["Camping"],
     },
     {
         "id": "35",
         "name": "Campingaz",
         "price": 20.0,
         "description": "",
-        "tags": [
-            "Camping"
-        ]
+        "tags": ["Camping"],
     },
     {
         "id": "36",
         "name": "Bombona butano sin usar (450g)",
         "price": 8.0,
         "description": "",
-        "tags": [
-            "Camping"
-        ]
+        "tags": ["Camping"],
     },
     {
         "id": "37",
         "name": "Hamaca Decathlon",
         "price": 20.0,
         "description": "",
-        "tags": [
-            "Camping"
-        ]
+        "tags": ["Camping"],
     },
     {
         "id": "38",
         "name": "Seat Ibiza 2008 125000 km",
         "price": 5000.0,
         "description": "",
-        "tags": [
-            ""
-        ]
+        "tags": [""],
     },
     {
         "id": "39",
         "name": "Televisor Panasonic 55\u201d 4K  + soporte pared",
         "price": 450.0,
         "description": "",
-        "tags": [
-            "Electr\u00f3nica",
-            "Muebles"
-        ]
+        "tags": ["Electr\u00f3nica", "Muebles"],
     },
     {
         "id": "40",
         "name": "Repetidor TP-Link (2)",
         "price": 50.0,
         "description": "",
-        "tags": [
-            "Electr\u00f3nica",
-            "Muebles"
-        ]
+        "tags": ["Electr\u00f3nica", "Muebles"],
     },
     {
         "id": "41",
         "name": "Mochila escolar 26L",
         "price": 20.0,
         "description": "",
-        "tags": [
-            ""
-        ]
+        "tags": [""],
     },
     {
         "id": "42",
         "name": "Mueble soporte monitor sobre-escritorio",
         "price": 15.0,
         "description": "",
-        "tags": [
-            ""
-        ]
-    }
+        "tags": [""],
+    },
 ]
 
 
 def add_initial_products(apps, schema_editor):
     for product in PRODUCTS_LIST:
-        Product = apps.get_model('products', 'Product')
+        Product = apps.get_model("products", "Product")
 
-        Product.objects.create(id=product['id'], name=product['name'], description=product['description'], price=product['price'])
+        Product.objects.create(
+            id=product["id"],
+            name=product["name"],
+            description=product["description"],
+            price=product["price"],
+        )
 
 
 def revert_migration(apps, schema_editor):
-    Product = apps.get_model('products', 'Product')
+    Product = apps.get_model("products", "Product")
 
     Product.objects.all().delete()
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('products', '0001_create_basic_products_model'),
+        ("products", "0001_create_basic_products_model"),
     ]
 
     operations = [
