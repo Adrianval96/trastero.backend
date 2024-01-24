6e5811b8fcfed39af5b88914cfb6ceaa3e2aef0b
Adrian V <adrianval96@gmail.com>
2024-01-24 17:11:27 +0100

    use real data on list endpoint

--- products/tests/test_retrieve_products.py
+++ products/tests/test_retrieve_products.py
@@ -1,17 +1,29 @@
+from unittest.mock import ANY
+
 import pytest
+
+from products.models import Product
 
 
 @pytest.mark.django_db
 class TestRetrieveProducts:
 
-    def test_foo(self, client):
+    def test_should_return_empty_list_when_no_products_exist(self, client):
+
+        response = client.get('/api/products/')
+
+        assert response.status_code == 200
+        assert response.json() == []
+
+    def test_should_return_products_list(self, client):
+        Product.objects.create(name='Opel Corsa', description='Vendo opel corsa', price=5.00)
 
         response = client.get('/api/products/')
 
         assert response.status_code == 200
         assert response.json() == [
             {
-                'id': 1,
+                'id': ANY,
                 'name': 'Opel Corsa',
                 'description': 'Vendo opel corsa',
                 'price': 5.00
--- products/views.py
+++ products/views.py
@@ -3,13 +3,11 @@ from http import HTTPStatus
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from products.models import Product
+
 
 class RetrieveProductsView(APIView):
-
     def get(self, request):
-        return Response(status=HTTPStatus.OK, data=[{
-            'id': 1,
-            'name': 'Opel Corsa',
-            'description': 'Vendo opel corsa',
-            'price': 5.00
-        }])
+        all_products = Product.objects.all()
+        response_data = [{'id': product.id, 'name': product.name, 'description': product.description, 'price': product.price} for product in all_products]
+        return Response(status=HTTPStatus.OK, data=response_data)
