1fc59d8a570ef6c204ce0509d60acc4e627e7819
Adrian V <adrianval96@gmail.com>
2024-01-25 00:44:16 +0100

    autouse ephemeral db

--- conftest.py
+++ conftest.py
@@ -2,8 +2,8 @@ import pytest
 from django.conf import settings
 
 
-@pytest.fixture
-def django_db_setup():
+@pytest.fixture(autouse=True)
+def ephemeral_test_db():
     settings.DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
