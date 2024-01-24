79e9ed5a13a34f658d869686507f9d279015d532
Adrian V <adrianval96@gmail.com>
2024-01-24 13:21:17 +0100

    only allow debug mode on local env

--- trastero/settings.py
+++ trastero/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'insecure_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_ENV') == 'local'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '.ew.r.appspot.com']
 
