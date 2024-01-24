5adb5a54fa4d9ddee60ff6fb495a2c2db849cb6b
Adrian V <adrianval96@gmail.com>
2024-01-24 13:14:21 +0100

    add django secret key

--- trastero/settings.py
+++ trastero/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(smr-rf_ipaqpufd0evcs$kyw*f&pjt8(v#qi75!o@-oz3h14@'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'insecure_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
